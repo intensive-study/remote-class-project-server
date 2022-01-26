2f458d95c889d4df1c0bdcfa5d74fdbc047a6908
zmfl1230 <zmfl123097@gmail.com>
2022-01-26 21:44:49 +0900

    feat: initialize project

---
+++ src/main/java/org/server/remoteclass/RemoteclassApplication.java
@@ -0,0 +1,13 @@
+package org.server.remoteclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemoteclassApplication.class, args);
+	}
+
+}
