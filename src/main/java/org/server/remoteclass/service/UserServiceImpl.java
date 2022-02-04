ad563176479d9ca586bf9288659bb9b66fd421cb
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-02-04 17:39:00 +0900

    return User -> UserDto로 변환

--- src/main/java/org/server/remoteclass/service/UserServiceImpl.java
+++ src/main/java/org/server/remoteclass/service/UserServiceImpl.java
@@ -10,10 +10,7 @@ import org.server.remoteclass.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
