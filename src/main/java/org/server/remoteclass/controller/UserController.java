83cad4915e012d5323c75e2862ab5c47aa29f8e0
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-02-04 16:11:36 +0900

    bugfix: findOneWithAUthoritiesByEmail 함수 대신 findByEmail 사용하게끔 수정

--- src/main/java/org/server/remoteclass/controller/UserController.java
+++ src/main/java/org/server/remoteclass/controller/UserController.java
@@ -1,30 +1,39 @@
 package org.server.remoteclass.controller;
 
 import org.server.remoteclass.dto.UserDto;
+import org.server.remoteclass.entity.User;
+import org.server.remoteclass.jpa.UserRepository;
 import org.server.remoteclass.service.UserService;
 import org.server.remoteclass.vo.RequestUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
     @Autowired
-    public UserController(UserService userService){
+    public UserController(UserService userService, UserRepository userRepository){
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome test";
     }
+
+//    @GetMapping("/myself")
+//    public ResponseEntity<UserDto> getUser(){
+//        return ResponseEntity.ok(userService.getMyUserWithAuthorities().get());
+//    }
 
 }
