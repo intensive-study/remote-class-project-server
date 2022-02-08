fac83a0041446db566fca1b41d5b309db79bb674
yijinKim <yijin3018@gmail.com>
2022-02-08 21:35:19 +0900

    bugfix : json date time format pr

--- src/main/java/org/server/remoteclass/dto/LectureFormDto.java
+++ src/main/java/org/server/remoteclass/dto/LectureFormDto.java
@@ -1,9 +1,11 @@
 package org.server.remoteclass.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.server.remoteclass.entity.Lecture;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
