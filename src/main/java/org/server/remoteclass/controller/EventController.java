fcb30db6976efbc4c244f505a4d457c6bccd7adf
박현우(Park Hyun Woo) <gusdn3477@naver.com>
2022-02-13 20:28:30 +0900

    feat: EventController @ApiOperation 구현

--- src/main/java/org/server/remoteclass/controller/EventController.java
+++ src/main/java/org/server/remoteclass/controller/EventController.java
@@ -1,5 +1,6 @@
 package org.server.remoteclass.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.server.remoteclass.dto.event.RequestEventDto;
 import org.server.remoteclass.dto.event.ResponseEventDto;
 import org.server.remoteclass.service.EventService;
@@ -20,22 +21,26 @@ public class EventController {
         this.eventService = eventService;
     }
 
+    @ApiOperation(value = "모든 이벤트 조회")
     @GetMapping
     public ResponseEntity<List<ResponseEventDto>> getAllEvents(){
         return ResponseEntity.status(HttpStatus.OK).body(eventService.getAllEvents());
     }
 
+    @ApiOperation(value = "이벤트 번호로 이벤트 조회")
     @GetMapping("/{eventId}")
     public ResponseEntity<ResponseEventDto> getEvent(@PathVariable("eventId") Long eventId){
         return ResponseEntity.status(HttpStatus.OK).body(eventService.getEvent(eventId));
     }
 
+    @ApiOperation(value = "이벤트 생성", notes = "이벤트 생성과 동시에 이벤트와 연계된 쿠폰을 생성한다.")
     @PostMapping
     public ResponseEntity createEvent(@RequestBody @Valid RequestEventDto requestEventDto){
         eventService.createEvent(requestEventDto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @ApiOperation(value = "이벤트 삭제", notes = "이벤트 번호를 파라미터로 넘겨 해당하는 이벤트를 삭제한다.")
     @DeleteMapping("/{eventId}")
     public ResponseEntity deleteEvent(@PathVariable("eventId") Long eventId){
         eventService.deleteEvent(eventId);
