999b43ff68cfc0d31045b53ef25d590d38b5dbe9
zmfl1230 <zmfl123097@gmail.com>
2022-02-20 00:30:42 +0900

    feat: exception advisor 글로벌하게 사용할 수 있도록 변경

--- src/main/java/org/server/remoteclass/exception/GlobalExceptionHandler.java
+++ src/main/java/org/server/remoteclass/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package org.server.remoteclass.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,22 +9,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(EmailDuplicateException.class)
-    public ResponseEntity<ExceptionResponse> handleEmailDuplicateException(EmailDuplicateException exception){
-        log.error("handleEmailDuplicateException", exception);
+    @ExceptionHandler(RemoteGlobalException.class)
+    public ResponseEntity<ExceptionResponse> handleRemoteException(RemoteGlobalException exception){
+        log.error("RemoteGlobalException", exception.getErrorCode().getMessage());
         ExceptionResponse response = new ExceptionResponse(exception.getErrorCode());
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(IdNotExistException.class)
-    public ResponseEntity<ExceptionResponse> handleIdNotExist(IdNotExistException exception){
-        ExceptionResponse response = new ExceptionResponse(exception.getErrorCode());
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler(ForbiddenException.class)
-    public ResponseEntity<ExceptionResponse> handleForbiddenException(ForbiddenException exception){
-        ExceptionResponse response = new ExceptionResponse(exception.getErrorCode());
-        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(response, exception.getErrorCode().getStatus());
     }
 }
