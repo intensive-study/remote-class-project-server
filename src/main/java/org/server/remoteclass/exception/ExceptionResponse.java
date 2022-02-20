bc898182d3c2177be45f3b8ae4a16312739eccb1
zmfl1230 <zmfl123097@gmail.com>
2022-02-20 16:12:54 +0900

    feat: 다이나믹하게 ExceptionResponse 생성할 수 있도록 생성자 정의

--- src/main/java/org/server/remoteclass/exception/ExceptionResponse.java
+++ src/main/java/org/server/remoteclass/exception/ExceptionResponse.java
@@ -14,4 +14,10 @@ public class ExceptionResponse {
         this.code = errorCode.getCode();
         this.message = errorCode.getMessage();
     }
+
+    public ExceptionResponse(String message, String code, HttpStatus status) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
 }
