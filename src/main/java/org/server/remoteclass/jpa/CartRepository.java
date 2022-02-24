46599a619aa3f0cfa05cc4acfa320997e3080106
zmfl1230 <zmfl123097@gmail.com>
2022-02-24 19:20:41 +0900

    refactor: [Cart] - 카트 조회시 쿼리 최적화 - jqpl lowercase로 변경 - cart에 담긴 총 금액 합 Join으로 변경

--- src/main/java/org/server/remoteclass/jpa/CartRepository.java
+++ src/main/java/org/server/remoteclass/jpa/CartRepository.java
@@ -9,6 +9,10 @@ import java.util.List;
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     //수강생 별 장바구니에 넣은 강좌 리스트 최근순으로
+    @Query("select c from Cart c " +
+            "join fetch c.lecture l " +
+            "join fetch l.user " +
+            "where c.user.userId=:userId")
     List<Cart> findByUser_UserIdOrderByCreatedDateDesc(Long userId);
     //수강생이 강좌를 수강했었는지 여부
     boolean existsByLecture_LectureIdAndUser_UserId(Long lectureId, Long userId);
@@ -18,10 +22,13 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     void deleteAllByUser_UserId(Long userId);
     boolean existsByUser_UserId(Long userId);
 
-    @Query("SELECT SUM(l.price) from Cart c, Lecture l where c.lecture.lectureId= l.lectureId and c.user.userId= :userId")
+    @Query("select sum(l.price) from Cart c " +
+            "join c.lecture l " +
+            "where c.user.userId=:userId")
     int findSumCartByUserId(Long userId);
 
-    @Query("SELECT COUNT(c) from Cart c where c.user.userId= :userId")
+    @Query("select count(c) from Cart c " +
+            "where c.user.userId=:userId")
     int findCountCartByUserId(Long userId);
 
 }
