8486e11c1e162b59fe95d343007985f9f840e7d9
zmfl1230 <zmfl123097@gmail.com>
2022-02-24 19:07:22 +0900

    refactor: [Lecture] - 필요 데이터 ResponseLectureDto, ResponseLectureFromStudentDto 추가 - 강좌 아이디로 조회, 카테고리 별 강의 조회, 전체 조회 쿼리 최적화

--- src/main/java/org/server/remoteclass/dto/lecture/ResponseLectureDto.java
+++ src/main/java/org/server/remoteclass/dto/lecture/ResponseLectureDto.java
@@ -18,7 +18,9 @@ public class ResponseLectureDto {
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private Long categoryId;
+    private String categoryName;
     private Long lecturer;
+    private String lecturerName;
 
     public static ResponseLectureDto from(Lecture lecture){
         if(lecture == null) return null;
@@ -30,7 +32,9 @@ public class ResponseLectureDto {
                 .startDate(LocalDateTime.from(lecture.getStartDate()))
                 .endDate(LocalDateTime.from(lecture.getEndDate()))
                 .categoryId(lecture.getCategory().getCategoryId())
+                .categoryName(lecture.getCategory().getCategoryName())
                 .lecturer(lecture.getUser().getUserId())
+                .lecturerName(lecture.getUser().getName())
                 .build();
     }
 }
--- src/main/java/org/server/remoteclass/jpa/LectureRepository.java
+++ src/main/java/org/server/remoteclass/jpa/LectureRepository.java
@@ -2,12 +2,29 @@ package org.server.remoteclass.jpa;
 
 import org.server.remoteclass.entity.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long>{
 
+    @Query("select l from Lecture l " +
+            "join fetch l.category " +
+            "join fetch l.user " +
+            "where l.lectureId=:lectureId")
+    Optional<Lecture> findById(Long lectureId);
+
     //카테고리별 강의 조회
+    @Query("select l from Lecture l " +
+            "join fetch l.category " +
+            "join fetch l.user " +
+            "where l.category.categoryId=:categoryId")
     List<Lecture> findByCategory_CategoryId(Long categoryId);
+
+    @Query("select l from Lecture l " +
+            "join fetch l.category " +
+            "join fetch l.user")
+    List<Lecture> findAll();
 
 }
