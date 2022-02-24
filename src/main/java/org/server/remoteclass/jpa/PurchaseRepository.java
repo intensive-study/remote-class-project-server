c0ec46ea5bef2428a415738d5a9fe7935cf12e31
zmfl1230 <zmfl123097@gmail.com>
2022-02-24 19:21:31 +0900

    refactor: [Purchase] - 구매 조회시 쿼리 최적화

--- src/main/java/org/server/remoteclass/jpa/PurchaseRepository.java
+++ src/main/java/org/server/remoteclass/jpa/PurchaseRepository.java
@@ -1,13 +1,23 @@
 package org.server.remoteclass.jpa;
 
-import org.server.remoteclass.constant.Authority;
 import org.server.remoteclass.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-//    @Query("select p from Purchase p where p.order.user.userId = ?1 order by p.purchaseDate DESC")
+
+    @Query("select p from Purchase p " +
+            "join fetch p.order o " +
+            "where o.user.userId=:userId " +
+            "order by p.purchaseDate desc")
     List<Purchase> findByOrder_User_UserIdOrderByPurchaseDateDesc(Long userId);
+
+    @Query("select p from Purchase p " +
+            "join fetch p.order " +
+            "where p.purchaseId=:purchaseId")
+    Optional<Purchase> findById(Long purchaseId);
+
 }
