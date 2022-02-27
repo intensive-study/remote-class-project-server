8392bd4bd69917edfa81812624a111b5d86a5f81
yijinKim <yijin3018@gmail.com>
2022-02-27 16:58:09 +0900

    fix : 쿠폰 입력 null이어도 조회 가능하도록 수정

--- src/main/java/org/server/remoteclass/jpa/OrderRepository.java
+++ src/main/java/org/server/remoteclass/jpa/OrderRepository.java
@@ -9,11 +9,11 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     // 회원이 주문한 주문 목록 최신순으로
-    @Query("select o from Order o " +
-            "join fetch o.issuedCoupon i " +
-            "join fetch i.coupon c " +
-            "where o.user.userId=:userId " +
-            "order by o.orderDate desc ")
+//    @Query("select o from Order o " +
+//            "join fetch o.issuedCoupon i " +
+//            "join fetch i.coupon c " +
+//            "where o.user.userId=:userId " +
+//            "order by o.orderDate desc ")
     List<Order> findByUser_UserIdOrderByOrderDateDesc(Long userId);
 
     List<Order> findByOrderByOrderDateDesc();
